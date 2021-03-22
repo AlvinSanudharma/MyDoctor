d2a97958cd3c6bd4973406050824dec4c77f0196
unknown <alvinsanudharma@gmail.com>
2021-03-22 09:18:13 +0800

    Slicing - Menambahkan Dummy Data

--- src/components/molecules/ListHospital/index.js
+++ src/components/molecules/ListHospital/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
-import {DummyDoctor2} from '../../../assets';
+import {DummyHospital1} from '../../../assets';
 import {colors, fonts} from '../../../utils';
 
-const ListDoctor = ({profile, name, desc}) => {
+const ListHospital = ({type, name, address, pic}) => {
   return (
     <View style={styles.container}>
-      <Image source={profile} style={styles.avatar} />
+      <Image source={pic} style={styles.picture} />
       <View>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.desc}>{desc}</Text>
+        <Text style={styles.title}>{type}</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.address}>{address}</Text>
       </View>
     </View>
   );
 };
 
-export default ListDoctor;
+export default ListHospital;
 
 const styles = StyleSheet.create({
   container: {
@@ -23,22 +24,22 @@ const styles = StyleSheet.create({
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: colors.border,
-    alignItems: 'center',
   },
-  avatar: {
-    width: 46,
-    height: 46,
-    borderRadius: 46 / 2,
-    marginRight: 12,
+  picture: {
+    width: 80,
+    height: 60,
+    borderRadius: 11,
+    marginRight: 16,
   },
-  name: {
+  title: {
     fontSize: 16,
     fontFamily: fonts.primary[400],
     color: colors.text.primary,
   },
-  desc: {
+  address: {
     fontSize: 12,
     fontFamily: fonts.primary[300],
     color: colors.text.secondary,
+    marginTop: 6,
   },
 });
