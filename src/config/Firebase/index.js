06b65d5a56e365f829e2a3895cb1f305bfb76b07
unknown <alvinsanudharma@gmail.com>
2021-03-26 10:39:41 +0800

    Integration - useForm dan Setup Firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBrhKzMsdxX4glcS9J47x0YXrd5Z-vMqM4',
+  authDomain: 'mydoctor-a4a6b.firebaseapp.com',
+  projectId: 'mydoctor-a4a6b',
+  storageBucket: 'mydoctor-a4a6b.appspot.com',
+  messagingSenderId: '415054055821',
+  appId: '1:415054055821:web:e21b0fc7125c73bf5415c4',
+});
+
+const Firebase = firebase;
+export default Firebase;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Header, Input, Button, Gap} from '../../components';
-import {colors} from '../../utils';
+import {colors, useForm} from '../../utils';
 
 const Register = ({navigation}) => {
+  const [form, setForm] = useForm({
+    fullName: '',
+    profession: '',
+    email: '',
+    password: '',
+  });
+
+  const onContinue = () => {
+    console.log(form);
+  };
+
   return (
     <View style={styles.page}>
       <Header onPress={() => navigation.goBack()} title="Daftat Akun" />
       <View style={styles.content}>
-        <Input label="Full Name" />
-        <Gap height={24} />
-        <Input label="Pekerjaan" />
-        <Gap height={24} />
-        <Input label="Email Address" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadFoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input
+            label="Full Name"
+            value={form.fullName}
+            onChangeText={value => setForm('fullName', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Pekerjaan"
+            value={form.profession}
+            onChangeText={value => setForm('profession', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Email Address"
+            value={form.email}
+            onChangeText={value => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={form.password}
+            onChangeText={value => setForm('password', value)}
+          />
+          <Gap height={40} />
+          <Button title="Continue" onPress={onContinue} />
+        </ScrollView>
       </View>
     </View>
   );
